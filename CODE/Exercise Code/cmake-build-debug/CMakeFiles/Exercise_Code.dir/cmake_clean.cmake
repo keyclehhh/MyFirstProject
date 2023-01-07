f2958ca98a753d8245b3d27aa4ad931cd673aa90
kuku <3103086541@qq.com>
2023-01-07 20:12:31 +0800

    Day1

---
+++ CODE/Exercise Code/cmake-build-debug/CMakeFiles/Exercise_Code.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Exercise_Code.dir/main.c.obj"
+  "Exercise_Code.exe"
+  "Exercise_Code.exe.manifest"
+  "Exercise_Code.pdb"
+  "libExercise_Code.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Exercise_Code.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CODE/Exercise Code/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+
+
+    int c=111;
+    printf("%d\n",c);//十进制输出
+    printf("%o\n",c);//八进制
+    printf("%x\n",c);//二进制
+    return 0;
+}
