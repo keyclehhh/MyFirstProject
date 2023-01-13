376829868ccb41f6e932bfc67a0db4f547f9e6c3
kuku <3103086541@qq.com>
2023-01-13 23:53:29 +0800

    repetition  drill

---
+++ repetition drill/cmake-build-debug/CMakeFiles/repetition_drill.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/repetition_drill.dir/main.cpp.obj"
+  "librepetition_drill.dll.a"
+  "repetition_drill.exe"
+  "repetition_drill.exe.manifest"
+  "repetition_drill.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/repetition_drill.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ repetition drill/main.cpp
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+    //int main() {
+    //   int i;
+    //   char j;
+    //   float k;
+    //   scanf("%d %c%f",&i,&j,&k);
+    //   float b=i+j+k;
+    //    printf("%0.2f\n",b);
+//    int main(){
+//    int a,b,c,d,count=0;//count记得要初始化为0
+//    for(a=1;a<10;a++){
+//        for(b=1;b<20;b++){
+//            for(c=1;c<50;c++){
+//                for(d=1;d<100;d++){
+//                    if(10*a+5*b+2*c+d==100&&a+b+c+d==40)
+//                    {count++;}
+//                }
+//            }
+//        }
+//    }
+//
+//        printf("%d\n",count);
+//    return 0;
+//}
