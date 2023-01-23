064a852ccdb8bd7af25dfda0baab92f9e94d7ff7
kuku <3103086541@qq.com>
2023-01-23 22:53:16 +0800

    stack and queue 2.0

---
+++ stack_and_queue_2.0/cmake-build-debug/CMakeFiles/Stack_and_queue.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Stack_and_queue.dir/main.cpp.obj"
+  "Stack_and_queue.exe"
+  "Stack_and_queue.exe.manifest"
+  "Stack_and_queue.pdb"
+  "libStack_and_queue.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Stack_and_queue.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
