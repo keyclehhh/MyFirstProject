eb35335132ba9c03ab7ee5afaebee18a2880401f
kuku <3103086541@qq.com>
2023-02-15 18:58:28 +0800

    合一

---
+++ 21/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char a[]="mov";//将第二个操作数复制到第一个操作数中
+    char b[]="push";//压栈
+    char c[]="pop";//出栈
+    printf("%3s\n",a);
+    printf("%3s\n",b);
+    printf("%3s\n",c);
+    return 0;
+}
